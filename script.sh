e131575622acc5187a3a5cf62b7a5849974b1d97
Deepak Sharma <ds9199027@gmail.com>
2023-04-10 10:06:26 +0530

    scrift file added..

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 
 deploy=sudo kubectl get deploy | awk 'NR==2 {print $1}'
-
+BUILD_TAG="$1"
 if [ "$deploy" = "myjavapp" ] ;
 then 
         echo "deployment exists"
 else 
          sudo wget https://raw.githubusercontent.com/deepaksharma2007/Java-Maven-K8s-Jenkins-Pipeline/master/deploy.yml
-         kubectl apply -f deploy.yml
+         kubectl apply -f deploy.yml ${BUILD_TAG}
 fi
