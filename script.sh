3c7f960f1df6d5b6c04a9d197f980042e877f69a
Deepak Sharma <ds9199027@gmail.com>
2023-04-10 10:17:22 +0530

    script

--- script.sh
+++ script.sh
@@ -7,5 +7,5 @@ then
         echo "deployment exists"
 else 
          sudo wget https://raw.githubusercontent.com/deepaksharma2007/Java-Maven-K8s-Jenkins-Pipeline/master/deploy.yml
-         kubectl apply -f deploy.yml ${BUILD_TAG}
+         sudo kubectl apply -f deploy.yml --prune --selector BUILD_TAG=${BUILD_TAG}
 fi
